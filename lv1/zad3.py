4d9c14f1acf9d91e0d683e822936f05427a1c64e
FabioMandic <168546186+FabioMandic@users.noreply.github.com>
2025-03-07 09:39:48 +0100

    Add files via upload

---
+++ lv1/zad3.py
@@ -0,0 +1,19 @@
+#3
+numbers=[]
+while(True):
+    try:
+        num=input('Input number:')
+        if num=='Done':
+            break
+        numbers.append(float(num))
+    except:
+        print("Not a number")
+print(f"Number inputed: {len(numbers)}")
+print(f"Average value: {sum(numbers)/len(numbers)}")
+print(f"Minimal value: {min(numbers)}")
+print(f"Maximal value: {max(numbers)}")
+
+numbers.sort()
+print(numbers)
+for number in numbers:
+    print(number)
